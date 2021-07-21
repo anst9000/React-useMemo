01c3ef91b1b251bba5e6df484ba259d7fafe9f22
Anders Strömberg <40022844+anst9000@users.noreply.github.com>
2021-07-21 12:15:32 +0200

    Finish example of useMemo All actual changes is in App.js, but added a sandbox.config.js file. Full example of useMemo() hook in React.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React, { useState, useMemo, useEffect } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? "black" : "white",
+      color: dark ? "white" : "black"
+    };
+  }, [dark]);
+
+  useEffect(() => {
+    console.log("Theme changed");
+  }, [themeStyles]);
+
+  const updateNumber = (e) => {
+    if (e.target.value === "") {
+      setNumber(0);
+    } else {
+      setNumber(parseInt(e.target.value, 10));
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <input type="number" value={number} onChange={(e) => updateNumber(e)} />
+      <button onClick={(_) => setDark((prevDark) => !prevDark)}>
+        Change theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
   );
+}
+
+function slowFunction(num) {
+  console.log("Calling Slow Function");
+  for (let i = 0; i < 500000000; i++) {}
+
+  return num * 2;
 }
